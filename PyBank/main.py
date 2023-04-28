9bf1e2891100f0b29a94954835a1d15f73bdff57
Deepika-GH <dpitchikala@yahoo.com>
2023-04-28 16:26:15 -0400

    work in progress

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,20 @@
-print("Hello")
+
+#Read the file from resources folder
+
+import os
+
+import csv
+
+budget_csv=os.path.join("Resources","budget_data.csv")
+
+#list to store file data
+
+date_Month=[]
+profit_losses=[]
+
+with open(budget_csv) as csvfile:
+    csvreader=csv.reader(csvfile,delimiter=",")
+    print(csvreader)
+    next
+
+    Total_Months=
